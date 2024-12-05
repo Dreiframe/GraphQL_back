1bf32f667b9209b79b152983403cb17e7e15d08f
aksu <scyzhe@hotmail.com>
2024-12-05 12:05:38 +0200

    preparing for db

---
+++ src/connection/psql.ts
@@ -0,0 +1,15 @@
+import pg from 'pg'
+const { Pool } = pg;
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: parseInt(process.env.DB_PORT ? process.env.DB_PORT : '5432'),
+});
+
+//https://node-postgres.com/features/pooling
